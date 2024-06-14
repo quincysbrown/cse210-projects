1529a1821caaa98ab91c81c536db736e6d3c9cab
Quincy Brown <quincyb2899@gmail.com>
2024-06-14 15:41:38 -0600

    2nd draft prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,9 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Beginning prompt.
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        // Creating a new list called numbers. Declaring variable number.
         List<int> numbers = new List<int>();
         int number;
+
+        // Do while loop to collect numbers from the user for the list.
         do
         {
             Console.Write("Enter a number: ");
@@ -20,8 +25,12 @@ class Program
 
         }
         while (number != 0);
+
+        // Declaring variables for upcoming calculations and determinations.
         int sum = 0;
         int min_pos_num = int.MaxValue;
+
+        // Calculating sum and minimum positive number by examining the list gathered from the user.
         foreach (int list_num in numbers)
         {
             sum = sum + list_num;
@@ -30,14 +39,20 @@ class Program
                 min_pos_num = list_num;
             }
         }
+
+        // Creating sorted list, calculating average, and determining the max number in the list.
         var sorted_set = numbers.OrderBy(num => num);
         double avg = numbers.Average();
         int max = numbers.Max();
+
+        // Printing values for calculations in the list.
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is: {avg}");
         Console.WriteLine($"The largest number is: {max}");
         Console.WriteLine($"The smallest positive number is: {min_pos_num}");
         Console.WriteLine("The sorted list is:");
+
+        // Printing the sorted list of numbers.
         foreach (int value in sorted_set)
         {
             Console.WriteLine(value);
