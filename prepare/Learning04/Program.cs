97852124313d14e72f453920eb35dfc66eafd8ac
Quincy Brown <quincyb2899@gmail.com>
2024-07-02 14:26:02 -0600

    Finished Learning04.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of Word War II by Mary Waters");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+        Console.WriteLine();
+
     }
 }
